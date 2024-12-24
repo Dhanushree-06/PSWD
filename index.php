a2aaaff85517e67b6db78f78ad5420c919a4719f
Dhanushree-06 <dhanushreeg.24cse@kongu.edu>
2024-12-24 11:53:36 +0530

    First project

---
+++ index.php
@@ -0,0 +1,15 @@
+
+<?php 
+
+$x = 10; 
+$y = 4; 
+
+// Performs Arithmetic operations 
+// and display the result 
+echo($x + $y) . "</br>"; 
+echo($x - $y) . "</br>"; 
+echo($x * $y) . "</br>"; 
+echo($x / $y) . "</br>"; 
+echo($x % $y); 
+
+?>
